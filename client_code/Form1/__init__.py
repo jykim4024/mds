158fee8870009c3debde7dc45556bb304acffe07
totorokjy@gmail.com <totorokjy@gmail.com>
2023-12-11 12:39:35 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,20 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def name_box_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def submit_btn_click(self, **event_args):
+    name = self.name_box.text
+    email = self.email_box.text
+    feedback = self.feedback_box.text
+    anvil.server.call('add_feedback',name,email,feedback)
+    Notification("Feedback submitted!").show()
+    self.clear_inputs()
+
+  def clear_inputs(self):
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
